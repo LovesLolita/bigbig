7b066cece386f3232dd261b5132f606899c8ef1d
BluesMa <maxiaowl@foxmail.com>
2021-03-19 21:16:52 +0800

    index首页完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,7 +4,14 @@ $.ajaxPrefilter(function (options) {
     //给有权限的接口
     if (options.url.indexOf('/my/') !== -1) {
         options.headers = {
-            Authorization: localStorage.getItem(key, 'token')
+            Authorization: localStorage.getItem('token')
+        }
+    }
+    //限制后台首页的访问权限
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login.html';
         }
     }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function () {
+    //弹层对象
+    var layer = layui.layer;
+
+    //获取用户信息
+    getUserInfo();
+
+    function getUserInfo() {
+        $.ajax({
+            method: 'get',
+            url: '/my/userinfo',
+            success: function (res) {
+                console.log(res);
+                if (res.status !== 0) {
+                    return layui.layer.msg(res.message, {
+                        icon: 5
+                    })
+                }
+                //渲染用户数据
+                renderAvatar(res.data);
+            },
+        })
+
+    }
+
+    function renderAvatar(user) {
+        //欢迎语(昵称或者用户名  昵称优先显示)
+        var name = user.nickname || user.username;
+        $('#welcome').html('欢迎你' + name);
+        if (user.user_pic) {
+            //渲染图片头像\文字头像隐藏
+            $('.layui-nav-img').attr('src', user.user_pic).show();
+            $('text-avatar').hide();
+        } else {
+            //
+            var first = name.substr(0, 1).toUpperCase();
+            $('.text-avatar').html(first).show();
+            $('.layui-nav-img').hide();
+
+        }
+    }
+    //退出登入
+    $('#logout').on('click', function () {
+        //询问退出
+        layer.confirm('退出吗', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //单击确定执行这个回调函数
+            localStorage.removeItem('token');
+            //跳转这个登入页面
+            location.href = '/login.html';
+            layer.close(index);
+        })
+    })
+})
